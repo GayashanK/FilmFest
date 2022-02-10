695d653eb0cec7fb3caebcf030308b28f8b35a7c
Kasun Gayashan <kgdharmasiri@gmail.com>
2022-02-10 08:41:24 +0000

    Initial Commit

---
+++ FilmFest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmFest
+//
+//  Created by Kasun Gayashan on 10.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
